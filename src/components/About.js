06146da8ef0772a8488bba3a66ba4157360afceb
thadeus <thadeusnyariki94@gmail.com>
2022-09-20 00:08:23 +0300

    Done assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id='about'>
+    <h2>About Me</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.
+    </p>
+    <img src={image} alt='I made this' />
+  </div>
+  );
 }
 
 export default About;
